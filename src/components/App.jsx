2400674db035de3363dacbb7a43182e49750ee53
Mykhailo Novikov <100936797+MichaelNovikov95@users.noreply.github.com>
2022-09-07 13:06:00 +0300

    add redirect to login/contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import Navigation from 'Navbar/Navbar';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 import { RegisterView, ContactView, LoginView } from 'Views';
 import { currentUser } from 'redux/auth/authOperations';
 import { useDispatch } from 'react-redux';
@@ -7,7 +7,15 @@ import { useEffect } from 'react';
 import { PublicRoute, PrivateRoute } from './Route/Route';
 
 export default function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (location.pathname === '/goit-react-hw-08-phonebook') {
+      navigate('/contacts');
+    }
+  }, [navigate, location.pathname]);
 
   useEffect(() => {
     dispatch(currentUser());
